08e59f7f67d6cc8e8a29e300aa7533f3b68f29e0
Ruba Vani <rubavanipitchaimari@gmail.com>
2025-07-16 19:04:47 +0530

    Add files via upload

---
+++ data_type_num.py
@@ -0,0 +1,35 @@
+x = 5
+x_type =  type(x)
+print(x, type(x))
+
+y = 22.2
+print(y, type(y))
+
+x = 3.5
+print(x, type(x))
+
+number = 10
+name = "ruba"
+initial = "R"
+
+print(number, type(number), name, type(name), initial, type(initial))
+
+
+is_today = True
+is_monday = False
+
+print(is_today, type(is_today), is_monday, type(is_monday)) # a type of variable
+
+x=2
+y=10
+x,y = y,x # swap here without swap option
+print(x,y)
+
+
+x=10
+y=5
+
+temp =x #temp = x -> temp = 10
+x =y
+y = temp
+print(x,y)
