b2bc8404697f5680ad6a924323e092edf3bb4b9f
Ruba Vani <rubavanipitchaimari@gmail.com>
2025-07-16 19:05:03 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,15 @@
+
+
+#
+n = int(input(" n: "))
+
+if n>=0:
+    print("positive number")
+    if n==0:
+        print("whole number")
+    else:
+        print("natural number")
+else:
+    print("negative number")
+
+
