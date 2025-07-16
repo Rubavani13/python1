c810eb4eaec6f61c8e85d85404755564375da106
Ruba Vani <rubavanipitchaimari@gmail.com>
2025-07-16 19:04:02 +0530

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,23 @@
+x= int(input("x: "))
+y= int(input("y: "))
+
+add_xy = x+y
+sub_xy = x-y
+multi_xy = x*y
+divid_xy = x/y
+x_power_y = x**y
+ques_xy = x//y  #reminder- quation
+ques_xy_2 = int(x/y)
+mod_xy = x%y
+
+print(f"add: {add_xy}")
+print(f"sub: {sub_xy}")
+print(f"multi: {multi_xy}")
+print(f"divid: {divid_xy}")
+print(f"pow: {x_power_y}")
+print(f"ques: {ques_xy}")
+print(f"ques 2: {ques_xy_2}")
+print(f"mod: {mod_xy}")
+
+
+# input: a,b,c -> print quadratic roots (x1, x2)
